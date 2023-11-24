4126c4e93402e2cbe948aab84126eb0683cc1d22
Prajwal Patil <prajwalpatilk@gmail.com>
2023-11-24 23:41:53 +0530

    Allow users to select shlokas based on the content

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import nltk  
 print("Loading...")
 from nltk.corpus import wordnet as wn 
-from nltk.corpus import wordnet
 
 import os
 import re
@@ -15,10 +14,6 @@ from urllib.request import urlopen
 
 nouns = {x.name().split('.', 1)[0] for x in wn.all_synsets('n')}
 
-print("     ===================================================================================")
-print("                                          VEDA-INTERPRETOR                                      ")
-print("     ===================================================================================\n")
-
 
 def get_links(item):
     duckDuckUrl = 'https://html.duckduckgo.com/html/'
@@ -28,6 +23,8 @@ def get_links(item):
     link_results = set()
     payload['q'] = item
     link_elements = []
+    with open('processed_data/synonym_filter.txt', "r", encoding="utf8") as f:
+        synonym_filter = [line.rstrip() for line in f]
     while link_elements == []:
         try:
             res = requests.post(duckDuckUrl, data=payload, headers=headers)
@@ -42,17 +39,32 @@ def get_links(item):
     time.sleep(random.randrange(2, 4))
     for i in link_elements:
         link = i.get("href")
-        link_results.add(link)
+        if link and not re.match(r".*duckduckgo.*", link, re.IGNORECASE) and link != '/html/':
+            flag = 1
+            for filter_word in synonym_filter:
+                if re.match(rf'.*{filter_word}.*',link,re.IGNORECASE):
+                    flag = 0
+                    break
+            if flag == 1:
+                link_results.add(link)
     link_list = list(link_results)
     # print("Links fetched, ", link_list)
     return link_list
 
 def getSynonyms(word):
+    with open('processed_data/synonym_filter.txt', "r", encoding="utf8") as f:
+        synonym_filter = [line.rstrip() for line in f]
     synonyms = []
-    for syn in wordnet.synsets(word):
+    for syn in wn.synsets(word):
         for i in syn.lemmas():
-            synonyms.append(i.name())
-
+            name = i.name()
+            flag = 1
+            for filter_word in synonym_filter:
+                if re.match(rf'.*{filter_word}.*',name,re.IGNORECASE):
+                    flag = 0
+                    break
+            if flag == 1:
+                synonyms.append(name)
     synonyms_new = [i.replace("_"," ").lower() for i in synonyms] 
     synonyms_new.append(word)
     return set(synonyms_new)
@@ -87,44 +99,130 @@ def searchTerms(sentence):
                 searchTerms.append(term)
     return searchTerms
 
-                    
-choice = int(input("MENU :\n1. Rig Veda\n2. Sama Veda\n3. Atharva Veda\n4. Yajur Veda\nEnter your choice : "))
-id = int(input("Enter Sholka id : "))
+def fetch_veda(choice):
+    fetched_data = []
+    veda="processed_data/"
+    if(choice == 1):
+        veda = veda + "rigveda_exp_final.txt"
+    elif(choice == 2):
+        veda = veda + "samveda_exp_final.txt"
+    elif(choice == 3):
+        veda = veda + "atharvaveda_exp_final.txt"
+    else :
+        veda = veda + "yajurveda_exp_final.txt"
+    with open(veda, "r", encoding="utf8") as f:
+        fetched_data = [line.rstrip() for line in f]
+    return fetched_data
 
-shloka = giveShloka(choice,id)
+def print_lines(data_array):
+    for i in data_array:
+        print(i)
 
-print("\n=================")
-print(shloka)
-print("=================\n")
+def linear_search(fetched_data):
+    while True:
+        candidate_shlokas = []
+        prompt_word = input("Enter the search term: ");
+        print("\nSearching..")
+        for i in fetched_data:
+            if re.search(rf'.*{prompt_word}.*',i,re.IGNORECASE):
+                candidate_shlokas.append(i);
+        print(f"Search results for {prompt_word} -\n")
+        print_lines(candidate_shlokas)
+        print("\n\n")
+        selection = int(input("\n1)Select a shloka from the search results\n2)Search again\n3)Exit\nEnter your choice: "))
+        if selection == 1:
+            index_selection = int(input("\nEnter the index of the shloka: "))
+            return index_selection
+        elif selection == 2:
+            continue
+        else: 
+            return -1;
+    return -1
 
-subsentences = shloka.split(",")
-terms = []
 
-for subsentence in subsentences:
-    terms = terms + searchTerms(subsentence)
-print("Search terms generated")
+def linear_search_prompted(fetched_data):
+    while True:
+        prompt_word = input("Enter the search term: ");
+        print("\nSearching..")
+        index = 0
+        for i in fetched_data:
+            if re.search(rf'.*{prompt_word}.*',i,re.IGNORECASE):
+                print(i)
+                print("\n")
+                selection = int(input("\n1)Select this shloka\n2)Next Shloka\n3)Exit\nEnter your choice: "))
+                print("\n")
+                if selection == 1:
+                    index = int(i.split(":")[0])
+                    return index
+                elif selection == 2:
+                    continue
+                else: 
+                    return -1;
+                print("\n\n")
+        selection_outer = int(input("\nNo more results to show\n1)Search again\n2)Exit\nEnter your choice: "))
+        if selection_outer == 1:
+            continue
+        else: 
+            return -1;
 
-print("Fetching links...\n")
 
-links = []
-i=1
-for term in terms:
-    if i==10:
-        break
-    print("Fetching links for " + term)
-    links = links + get_links(term)
-    i=i+1
-
-freq = {}
-for item in links:
-    if (item in freq):
-        freq[item] += 1
+def main():
+    print("===================================================================================")
+    print("                                     VEDA-INTERPRETOR                              ")
+    print("===================================================================================\n")
+    choice = int(input("MENU :\n1. Rig Veda\n2. Sama Veda\n3. Atharva Veda\n4. Yajur Veda\nEnter your choice : "))
+    entry_choice = int(input("How would you like to proceed with the search?\n1)Search by keyword\n2)Search by Shloka id\nEnter your choice: "))
+    if entry_choice == 1:
+        print("Fetching data..")
+        all_data = fetch_veda(choice)
+        print("Data fetched.")
+        # shloka_index = linear_search(all_data)
+        shloka_index = linear_search_prompted(all_data)
+        if shloka_index > 0:
+            shloka = all_data[shloka_index - 1]
+        else:
+            print("Exiting!")
+            return
     else:
-        freq[item] = 1
+        id = int(input("Enter Sholka id : "))
+        shloka = giveShloka(choice,id)
+        
 
-print("Results : ")
-freq = sorted(freq.items(), key=lambda x:x[1], reverse=True)
-for x in freq:
-    print(x)
 
-# high-priest of yajna and the divine voice
+    print("\n=================")
+    print(shloka)
+    print("=================\n")
+
+    subsentences = shloka.split(",")
+    terms = []
+
+    for subsentence in subsentences:
+        terms = terms + searchTerms(subsentence)
+    print("Search terms generated")
+
+    print("Fetching links...\n")
+
+    links = []
+    i=1
+    for term in terms:
+        if i==10:
+            break
+        print("Fetching links for " + term)
+        links = links + get_links(term)
+        i=i+1
+
+    freq = {}
+    for item in links:
+        if (item in freq):
+            freq[item] += 1
+        else:
+            freq[item] = 1
+
+    print("Results : ")
+    freq = sorted(freq.items(), key=lambda x:x[1], reverse=True)
+    for x in freq:
+        print(x)
+
+
+if __name__ == "__main__":
+    main()
